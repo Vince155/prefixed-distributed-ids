49f95696749c0a031b8ff0f86e15a32eafafc44b
Vince155 <vincent.onochie1@gmail.com>
2023-07-22 14:54:56 -0500

    fixed tests (#2)

--- number_generator/number_generator_test.go
+++ number_generator/number_generator_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func TestNumberIsGenerated(t *testing.T) {
-    value, err := generateBytes()
+    value, err := GenerateBytes()
 
     assert.NotEqual(t, 0, value, "value should not be -1")
     assert.Equal(t, err, nil, "Error should be nil")
 }
 
 func TestNumbersGeneratedAreUnique(t *testing.T) {
-    value1, err1 := generateBytes()
-    value2, err2 := generateBytes()
+    value1, err1 := GenerateBytes()
+    value2, err2 := GenerateBytes()
 
     assert.NotEqual(t, value1, value2, "The two values should be different")
     assert.Equal(t, err1, err2, "Both errors should be nil")
