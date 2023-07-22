dfafac12d3a2cddc481f06083c4a1820f9d3210f
Vince155 <vincent.onochie1@gmail.com>
2023-07-22 15:02:14 -0500

    added counter generator tests (#3)

--- number_generator/number_generator_test.go
+++ number_generator/number_generator_test.go
@@ -20,3 +20,18 @@ func TestNumbersGeneratedAreUnique(t *testing.T) {
     assert.NotEqual(t, value1, value2, "The two values should be different")
     assert.Equal(t, err1, err2, "Both errors should be nil")
 }
+
+func TestCounterIsGenerated(t *testing.T) {
+    value, err := InitializeCounter()
+
+    assert.NotEqual(t, -1, value, "Value should not be -1")
+    assert.Equal(t, err, nil, "Error should be nil")
+}
+
+func TestCountersAreUnique(t *testing.T) {
+    value1, err1 := InitializeCounter()
+    value2, err2 := InitializeCounter()
+
+    assert.NotEqual(t, value1, value2, "The two values should be different")
+    assert.Equal(t, err1, err2, "Both errors should be nil")
+}
