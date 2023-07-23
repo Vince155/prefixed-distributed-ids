d57d44ebb0828772f35ef24d989b57d85b3320b9
Vince155 <vincent.onochie1@gmail.com>
2023-07-23 14:06:17 -0500

    unique id is generated each time (#4)

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ The ObjectId consists of the following:
 
 - A random value generated from a cryptographically random number generator. The value is unique to the machine - 5 bytes
 
-- An incrementing counter. Initialized to a random value - 3 bytes
+- An incrementing counter. Initialized to a random value - 2 bytes
 
-Those elements will combine to generate a 12-byte unique ID from which a prefix can be added e.g.
+Those elements will combine to generate a 11-byte unique ID from which a prefix can be added e.g.
--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"prefixed-distributed-ids/number_generator"
+	"prefixed-distributed-ids/timestamp_generator"
 )
 
 func main() {
-	counter, err := number_generator.InitializeCounter()
+	idBytes, id, timestamp := BuildId()
+
+    fmt.Printf("bytes are %x\n", idBytes)
+    fmt.Printf("timestamp is %d\n", timestamp)
+    fmt.Printf("id is %s\n", id)
+}
+
+func BuildId() ([]byte, string, int64) {
+    var idBytes []byte
+    tBuffer, timestamp := timestamp_generator.GenerateTimestampNumber()
+
+    idBytes = append(idBytes, tBuffer[0], tBuffer[1], tBuffer[2], tBuffer[3])
+
+    processBuffer, err := number_generator.GenerateBytes()
 
 	if err != nil {
 		panic(err)
 	}
 
-	for counter < 1000 {
-		counter++
+    idBytes = append(idBytes, processBuffer[0], processBuffer[1], processBuffer[2], processBuffer[3], processBuffer[4])
+
+    counter, err := number_generator.InitializeCounter()
+	counterBuffer := make([]byte, 2)
+	binary.BigEndian.PutUint16(counterBuffer, uint16(counter))
+
+    if err != nil {
+		panic(err)
 	}
 
-	fmt.Println("Hello, world")
+    idBytes = append(idBytes, counterBuffer[0], counterBuffer[1])
+    id := fmt.Sprintf("%x", idBytes)
+
+    return idBytes, id, timestamp
 }
